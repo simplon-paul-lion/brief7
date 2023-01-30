8464355bd1b53f5e96660b8f12a6302a5b667cd5
Paul <simplon-paul-lion@gmail.com>
2023-01-30 11:03:09 +0100

    ajout de variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "prefix" {
+  default = "BF7_paul"
+}
+
+variable "location" {
+  default = "francecentral"
+}
+
+variable "subid" {
+  default = "a1f74e2d-ec58-4f9a-a112-088e3469febb"
+
+}
+
+variable "admin" {
+  default = "paul"
+}
